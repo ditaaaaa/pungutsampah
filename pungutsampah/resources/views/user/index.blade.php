88767e0ae2f45953b2801c38e561680965ae8d24
ariwardana28 <36886965+ariwardana28@users.noreply.github.com>
2019-08-25 01:42:51 +0800

    S

--- pungutsampah/resources/views/user/index.blade.php
+++ pungutsampah/resources/views/user/index.blade.php
@@ -33,9 +33,9 @@
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item mx-0 mx-lg-1">
-            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Katalog Kerajinan</a>
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#kerajinan">Katalog Kerajinan</a>
 
-            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#kerajinan">Kerajinan Tangan</a>
+          
 
           </li>
           <li class="nav-item mx-0 mx-lg-1">
